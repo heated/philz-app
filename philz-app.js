1bc0a13695ecf9e1e8ebbdaf3a477be7db8071af
dvirdi <dvirdi@scu.edu>
2015-05-30 18:03:35 -0700

    meteor setup

---
+++ philz-app.js
@@ -0,0 +1,31 @@
+// simple-todos.js
+Answers = new Mongo.Collection("answers");
+
+if (Meteor.isClient) {
+  // This code only runs on the client
+  Template.body.helpers({
+    answers: function () {
+      return Answers.find({});
+    }
+  });
+
+  // Inside the if (Meteor.isClient) block, right after Template.body.helpers:
+  Template.body.events({
+    "submit .new-answer": function (event) {
+      // This function is called when the new answer form is submitted
+
+      var text = event.target.text.value;
+
+      Answers.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+
+      // Clear form
+      event.target.text.value = "";
+
+      // Prevent default form submit
+      return false;
+    }
+  });
+}
